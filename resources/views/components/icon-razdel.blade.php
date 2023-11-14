b05cbfb4d3a5a21a1bba7fb2fa382dc07f6808e3
dimaarion <dimaarion@gmail.com>
2023-11-14 08:18:03 +0200

    dimaarion/vin.sandaniprim

--- resources/views/components/icon-razdel.blade.php
+++ resources/views/components/icon-razdel.blade.php
@@ -4,5 +4,5 @@
         <path d="M43 6C43 3.23858 45.2386 1 48 1C50.7614 1 53 3.23858 53 6C53 8.76142 50.7614 11 48 11C45.2386 11 43 8.76142 43 6Z" id="Овал" fill="currentColor" fill-rule="evenodd" stroke="none"/>
         <path d="M64 5L94 5L94 8L64 8L64 5Z" id="Прямоугольник" fill="currentColor" fill-rule="evenodd" stroke="none"/>
         <path d="M0 5L30 5L30 8L0 8L0 5Z" id="Прямоугольник-2" fill="currentColor" fill-rule="evenodd" stroke="none"/>
-    </g>uyukyukyukyukyuk
+    </g>
 </svg>
